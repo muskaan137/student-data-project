472246470858f7060a85bec83e158d816b879d82
muskaan137 <muskaansharmahm@gmail.com>
2025-02-08 17:50:17 +0530

    Add files via upload

---
+++ student data.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void){
+int studentID=15;
+int studentAge=23;
+float studentFee=75.25;
+char studentGrade='b';
+printf("\n id of student is %d",studentID);
+printf("\n age of student is %d",studentAge);
+printf("\n fees of student is %f",studentFee);
+printf("\n grade of student is %c",studentGrade);
+return 0;
+}
+
+
+
